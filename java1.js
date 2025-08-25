f2a469122dee60bb1a6831f70bbb258c6cc2ff41
Muhammad Salman Ansar <salmanwattoo79@gmail.com>
2025-08-25 11:18:32 +0500

    Add Code

---
+++ java1.js
@@ -0,0 +1,66 @@
+//   Chapter Number 1 -------
+
+
+alert ("Hello.....! World")
+
+alert ("Hello World " +"\n" +"this is new line")
+
+alert ("`users` this is 3rd alart" +"\n" +"   space between line break")
+
+// Chapter Number 2 ------
+
+
+myname = "Anas";
+
+alert (myname)
+
+
+let Name = "Anas";
+let Class = "Web-development";
+let Subject = "Java Script";
+
+alert (Name +"\n" +Class + "\n" +Subject);
+
+
+myemail = "muhammadanas692003@gmail.com"
+
+message = "My Email Address is: " + myemail ;
+
+alert (message);
+
+
+
+document.write ("<P>I am reading the book 'A Smarter Way to Learn JavaScript.' It's a great resource for learning JavaScript.</P>" +"\n");
+
+
+//   Chapter Number 3 ------------------
+
+
+let nameofvisitor = "Anas"
+let product44 = "Computer"
+let quantity = "2"
+
+message = nameofvisitor + " ordered " + quantity + " " + product44 + " on local computer store.";
+
+document.write (message);
+
+
+//   Chapter Number 4 -------------------
+
+document.write("Rules for naming JS variables");
+document.write("Variable names can only contain letters, numbers, $ and _.");
+document.write("Variables must begin with a letter, $ or _.");
+document.write("Variable names are case sensitive");
+document.write("Variable names should not be JS reserved 1"); 
+
+
+//   Chapter Number 5 ---------------------
+
+nun11 = 2;
+num12 = 3;
+
+result = nun11 + num12;
+
+document.write ("the sum of" + num11  +"and" + num12  +"is equal to =" +result )
+
+
